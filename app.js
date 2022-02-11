d7f2cfa22e96b4ee36f4d0214fc015cb2a2cc97c
sainipankaj15 <pksmks1999@gmail.com>
2022-02-11 22:32:52 +0530

    complete-ready-now

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const res = require('express/lib/response');
 
 const app = express();
-const port = 8001;
+const port = 8000;
 
 //EXPRESS STUFF
 app.use('/static', express.static('static')); // serving static file 
 
+// To extract the data from the website to the app.js file, we have to write-
+app.use(express.urlencoded())
 
 //PUG STUFF
 app.set('view engine', 'pug'); //set template engine as a pug
@@ -16,9 +20,21 @@ app.set('views', path.join(__dirname, 'views')); // set the views folder
 app.get("/", (req, res) => {
 
     res.status(200).render('index.pug');
-
 });
 
+
+app.post("/",(req,res)=>{
+
+    let name = req.body.Name;
+    let gender = req.body.Gender;
+    let age = req.body.Age;
+    let address = req.body.address;
+    let alloutputdata = `Client name is ${name} with gender ${gender} and his/her age is ${age} his/her address is ${address} and more about him/her is`;
+    fs.writeFileSync('database.txt',alloutputdata);
+
+    const messaged = { 'message' : "Your form has submitted successfully"};
+    res.render('index.pug',messaged)
+});
 
 
 
